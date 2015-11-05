35d0fda1890064eb589c925a745f34464815535c
revcoder93 <revcoder93@gmail.com>
2015-11-05 22:01:09 +0200

    Kalaaaaam

--- Project1/src/tree/Node.java
+++ Project1/src/tree/Node.java
@@ -33,4 +33,16 @@ public class Node {
 	public boolean hasChildren(){
 		return children.size() > 0;
 	}
+	public boolean isEqual(Tile[][] c1) {
+		for (int i = 0; i < c1.length; i++) {
+			for (int j = 0; j < c1[i].length; j++) {
+				Tile t1 = c1[i][j];
+				Tile t2 = this.getConfiguration()[i][j];
+				if (t1.getId() != t2.getId()) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
--- Project1/src/tree/Tree.java
+++ Project1/src/tree/Tree.java
@@ -7,82 +7,84 @@ import tiles.Tile;
 public class Tree {
 	Node root;
 	ArrayList<Node> stateSpace;
+
 	public Tree(Node n) {
 		root = n;
 		stateSpace = new ArrayList<Node>();
 		stateSpace.add(root);
 	}
-	/*Checks if two configurations are equal*/
-	public boolean isEqual(Tile[][] c1, Tile[][] c2) {
-		for (int i = 0; i < c1.length; i++) {
-			for (int j = 0; j < c1[i].length; j++) {
-				Tile t1 = c1[i][j];
-				Tile t2 = c2[i][j];
-				if (t1.getId() != t2.getId()) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-	public void expand(Node parent){
+
+	public void expand(Node parent) {
 		Tile[][] c1 = parent.getConfiguration();
 		for (int i = 0; i < c1.length; i++) {
 			for (int j = 0; j < c1[0].length; j++) {
-				if(c1[i][j].isBlank()){
+				if (c1[i][j].isBlank()) {
 					ArrayList<Tile[][]> moves = move(c1, i, j);
-					for(int k = 0; k<moves.size(); k++){
+					for (int k = 0; k < moves.size(); k++) {
 						Node n = new Node(moves.get(k));
 						n.setParent(parent);
-						stateSpace.add(n);
+						Node n1 = parent;
+						while (true) {
+							if (n1 != null) {
+								if (n1.isEqual(n.getConfiguration())) {
+									break;
+								} else {
+									n1 = n1.parent;
+								}
+							} else {
+								stateSpace.add(n);
+								break;
+							}
+						}
 					}
 				}
 			}
 		}
 	}
+
 	/* Moves and returns an arraylist of the new configurations */
-	 public ArrayList<Tile[][]> move(Tile[][] configuration, int i, int j) {
-		 Tile blank = configuration[i][j];
-		 Tile temp;
-		 Tile[][] c2 = configuration;
-		 ArrayList<Tile[][]> ret = new ArrayList<Tile[][]>();
-		 if(i > 0){
-			 temp = configuration[i-1][j];
-			 if(temp.isBlock() || (temp.isPath() && !temp.isFixed())){
-				 c2[i][j] = temp;
-				 c2[i-1][j] = blank;
-				 ret.add(c2);
-			 }
-		 }
-		 c2 = configuration;
-		 if(i < configuration.length-1){
-			 temp = configuration[i+1][j];
-			 if(temp.isBlock() || (temp.isPath() && !temp.isFixed())){
-				 c2[i][j] = temp;
-				 c2[i+1][j] = blank;
-				 ret.add(c2);
-			 }
-		 }
-		 c2 = configuration;
-		 if(j > 0){
-			 temp = configuration[i][j-1];
-			 if(temp.isBlock() || (temp.isPath() && !temp.isFixed())){
-				 c2[i][j] = temp;
-				 c2[i][j-1] = blank;
-				 ret.add(c2);
-			 }
-		 }
-		 c2 = configuration;
-		 if(i < configuration[i].length-1){
-			 temp = configuration[i][j+1];
-			 if(temp.isBlock() || (temp.isPath() && !temp.isFixed())){
-				 c2[i][j] = temp;
-				 c2[i][j+1] = blank;
-				 ret.add(c2);
-			 }
-		 }
-		 return ret;
-	 }
+	public ArrayList<Tile[][]> move(Tile[][] configuration, int i, int j) {
+		Tile blank = configuration[i][j];
+		Tile temp;
+		Tile[][] c2 = configuration;
+		ArrayList<Tile[][]> ret = new ArrayList<Tile[][]>();
+		if (i > 0) {
+			temp = configuration[i - 1][j];
+			if (temp.isBlock() || (temp.isPath() && !temp.isFixed())) {
+				c2[i][j] = temp;
+				c2[i - 1][j] = blank;
+				ret.add(c2);
+			}
+		}
+		c2 = configuration;
+		if (i < configuration.length - 1) {
+			temp = configuration[i + 1][j];
+			if (temp.isBlock() || (temp.isPath() && !temp.isFixed())) {
+				c2[i][j] = temp;
+				c2[i + 1][j] = blank;
+				ret.add(c2);
+			}
+		}
+		c2 = configuration;
+		if (j > 0) {
+			temp = configuration[i][j - 1];
+			if (temp.isBlock() || (temp.isPath() && !temp.isFixed())) {
+				c2[i][j] = temp;
+				c2[i][j - 1] = blank;
+				ret.add(c2);
+			}
+		}
+		c2 = configuration;
+		if (i < configuration[i].length - 1) {
+			temp = configuration[i][j + 1];
+			if (temp.isBlock() || (temp.isPath() && !temp.isFixed())) {
+				c2[i][j] = temp;
+				c2[i][j + 1] = blank;
+				ret.add(c2);
+			}
+		}
+		return ret;
+	}
 
 	public boolean goalTest(Tile[][] configuration) {
 		// search for the initial tile
