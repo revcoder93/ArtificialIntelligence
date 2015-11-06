89c86ff489c2e929e61be923ab86b9bf8e82fee3
mansoursaid001 <mansoursaid001@hotmail.com>
2015-11-06 08:16:46 +0200

    Omar

--- Project1/src/main/RTB.java
+++ Project1/src/main/RTB.java
@@ -1,14 +1,38 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Queue;
+import java.util.Stack;
 import tiles.Tile;
 import tree.Node;
 import tree.Tree;
 
 public class RTB extends SearchProblem {
+	ArrayList<Node> children;
+	ArrayList<Node> result;
+	Queue<Node> queueBfs;
+	Stack<Node> stackDfs;
+	Stack<Node> stackId;
+	int limit;
+
+	public RTB(Node node, String strategy, boolean visualize) {
+		search(node.getConfiguration(), strategy, visualize);
+	}
+
+	public ArrayList<Node> fillResult(Node x, ArrayList<Node> a) {
+		if (x.getParent() == null) {
+			a.add(x.getParent());
+			return a;
+		} else {
+			a.add(x.getParent());
+			fillResult(x.getParent(), a);
+		}
+		return a;
+	}
 
 	@Override
 	public void search(Tile[][] grid, String strategy, boolean visualize) {
-
 		switch (strategy) {
 		case "BFS":
 			breadthFirstSearch(grid, visualize);
@@ -17,7 +41,7 @@ public class RTB extends SearchProblem {
 			depthFirstSearch(grid, visualize);
 			break;
 		case "ID":
-			iterativeDeepening(grid, visualize);
+			iterativeDeepening(grid, visualize, limit);
 			break;
 		case "A*":
 			aStar(grid, visualize);
@@ -32,29 +56,107 @@ public class RTB extends SearchProblem {
 
 	private void greedy(Tile[][] grid, boolean visualize) {
 		// TODO Auto-generated method stub
-		Node node = new Node(grid);
-		Tree tree = new Tree(node);
-		//et3amel w fe kolo bardo e3mel el 2 steps elly foo2 dool 3shan ykoon m3ak tree
-		
+
 	}
 
 	private void aStar(Tile[][] grid, boolean visualize) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	private void iterativeDeepening(Tile[][] grid, boolean visualize) {
-		// TODO Auto-generated method stub
+	private void iterativeDeepening(Tile[][] grid, boolean visualize, int limit) {
+		int limitNow = 0;
+		while (limitNow <= limit) {
+			int levels = 0;
+			Node nodeId = new Node(grid);
+			Tree treeId = new Tree(nodeId);
+			stackId.push(treeId.getRoot());
+			while (!stackDfs.isEmpty()) {
+				Node node = (Node) stackId.pop();
+				if (node.goalTest()) {
+					System.out.println("the sequence using Breadth First Search is: ");
+					result = fillResult(node, result);
+					Collections.reverse(result);
+					for (int j = 0; j < result.size(); j++) {
+						result.get(j).print();
+					}
+					System.out.println("");
 
+					return;
+				} else {
+					if (levels < limitNow) {
+						children = node.getChildren();
+						levels++;
+						for (int k = 0; k < children.size(); k++) {
+							stackId.push(children.get(k));
+						}
+					}
+				}
+			}
+			limitNow++;
+		}
+		children = null;
+		result = null;
 	}
 
 	private void depthFirstSearch(Tile[][] grid, boolean visualize) {
-		// TODO Auto-generated method stub
-
+		Node nodeDfs = new Node(grid);
+		Tree treeDfs = new Tree(nodeDfs);
+		stackDfs.push(treeDfs.getRoot());
+		while (!stackDfs.isEmpty()) {
+			Node node = (Node) stackDfs.pop();
+			if (node.goalTest()) {
+				System.out.println("the sequence using Breadth First Search is: ");
+				result = fillResult(node, result);
+				Collections.reverse(result);
+				for (int i = 0; i < result.size(); i++) {
+					result.get(i).print();
+				}
+				System.out.println("");
+				return;
+			} else {
+				children = node.getChildren();
+				for (int i = 0; i < children.size(); i++) {
+					stackDfs.push(children.get(i));
+				}
+			}
+			break;
+		}
+		children = null;
+		result = null;
 	}
 
 	private void breadthFirstSearch(Tile[][] grid, boolean visualize) {
-		// TODO Auto-generated method stub
+		Node nodeBfs = new Node(grid);
+		Tree treeBfs = new Tree(nodeBfs);
+		queueBfs.offer(treeBfs.getRoot());
+		while (!queueBfs.isEmpty()) {
+			Node node = (Node) queueBfs.poll();
+			if (node.goalTest()) {
+				System.out.println("the sequence using Breadth First Search is: ");
+				result = fillResult(node, result);
+				Collections.reverse(result);
+				for (int i = 0; i < result.size(); i++) {
+					result.get(i).print();
+				}
+				System.out.println("");
+				return;
+			} else {
+				children = node.getChildren();
+				for (int i = 0; i < children.size(); i++) {
+					queueBfs.offer(children.get(i));
+				}
+			}
+		}
+		children = null;
+		result = null;
+	}
 
+	public static void main(String[] args) {
+		Node n = new Node(Main.genGrid());
+		Tree t = new Tree(n);
+		t.getRoot().print();
+		System.out.println("---------");
+		t.getRoot().getChildren().get(1).print();
 	}
 }
--- Project1/src/tree/Node.java
+++ Project1/src/tree/Node.java
@@ -69,6 +69,91 @@ public class Node {
 		}
 	}
 
+	public boolean goalTest() {
+		// search for the initial tile
+		Tile initialTile = null;
+		int initialTileI = 0;
+		int initialTileJ = 0;
+		for (int i = 0; i < configuration.length; i++) {
+			for (int j = 0; j < configuration[i].length; j++) {
+				if (configuration[i][j].isInitial()) {
+					initialTile = configuration[i][j];
+					initialTileI = i;
+					initialTileJ = j;
+					break;
+				}
+			}
+		}
+		if (initialTile == null)
+			return false;
+
+		// iterate to find if they are connected
+		Tile currentTile = initialTile;
+		int currentTileI = initialTileI;
+		int currentTileJ = initialTileJ;
+		while (true) {
+			String direction = currentTile.getOut();
+			switch (direction) {
+			case "North":
+				if ((currentTileI - 1) > 0) {
+					if (configuration[currentTileI - 1][currentTileJ].getIn().equals("South")) {
+						if (configuration[currentTileI - 1][currentTileJ].isGoal())
+							return true;
+						currentTile = configuration[currentTileI - 1][currentTileJ];
+						currentTileI = currentTileI - 1;
+						continue;
+					} else {
+						return false;
+					}
+				} else {
+					return false;
+				}
+			case "South":
+				if ((currentTileI + 1) < configuration.length) {
+					if (configuration[currentTileI + 1][currentTileJ].getIn().equals("North")) {
+						if (configuration[currentTileI + 1][currentTileJ].isGoal())
+							return true;
+						currentTile = configuration[currentTileI + 1][currentTileJ];
+						currentTileI = currentTileI + 1;
+						continue;
+					} else {
+						return false;
+					}
+				} else {
+					return false;
+				}
+			case "East":
+				if ((currentTileJ + 1) < configuration[currentTileI].length) {
+					if (configuration[currentTileI][currentTileJ + 1].getIn().equals("West")) {
+						if (configuration[currentTileI][currentTileJ + 1].isGoal())
+							return true;
+						currentTile = configuration[currentTileI][currentTileJ + 1];
+						currentTileJ = currentTileJ + 1;
+						continue;
+					} else {
+						return false;
+					}
+				} else {
+					return false;
+				}
+			case "West":
+				if ((currentTileJ - 1) > 0) {
+					if (configuration[currentTileI][currentTileJ - 1].getIn().equals("East")) {
+						if (configuration[currentTileI][currentTileJ - 1].isGoal())
+							return true;
+						currentTile = configuration[currentTileI][currentTileJ - 1];
+						currentTileJ = currentTileJ - 1;
+						continue;
+					} else {
+						return false;
+					}
+				} else {
+					return false;
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		Node n = new Node(Main.genGrid());
 		n.print();
